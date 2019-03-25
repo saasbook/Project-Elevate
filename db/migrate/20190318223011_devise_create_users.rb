e4774dbf62937fdd7026f4e3cc434809cc08fc01
Zac <zdehkordi@berkeley.edu>
2019-03-25 15:37:56 -0700

    fixing kennys bugs

--- db/migrate/20190318223011_devise_create_users.rb
+++ db/migrate/20190318223011_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       t.string :nickname,           null: false, default: ""
+      t.string :membership,           null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email, :nickname      unique: true
+    add_index :users, :email,                unique: true             
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,15 @@ ActiveRecord::Schema.define(version: 2019_03_18_223011) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "nickname", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "membership", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
