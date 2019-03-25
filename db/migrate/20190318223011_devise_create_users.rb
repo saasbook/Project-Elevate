753bc8460d0453d69045a5a98fbe0770aff3092d
Zac <zdehkordi@berkeley.edu>
2019-03-25 15:49:09 -0700

    updated migration to have club member default

--- db/migrate/20190318223011_devise_create_users.rb
+++ db/migrate/20190318223011_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       t.string :nickname,           null: false, default: ""
-      t.string :membership,           null: false, default: ""
+      t.string :membership,           null: false, default: "Club Member"
 
       ## Recoverable
       t.string   :reset_password_token
