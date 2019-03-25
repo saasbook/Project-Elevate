1de89d1834c01492972c73715211c94d12857702
Puya Moayer <moayerpuya@gmail.com>
2019-03-24 18:35:12 -0700

    testing attributes

--- db/migrate/20190318223011_devise_create_users.rb
+++ db/migrate/20190318223011_devise_create_users.rb
@@ -7,6 +7,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :nickname,           null: false, default: ""
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -36,7 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, :nickname      unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
