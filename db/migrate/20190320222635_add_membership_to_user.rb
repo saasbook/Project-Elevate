bbfbf35ec1a082a4fd9f919f2b05dafaaf828d09
Joe Chen <chenjoe569@berkeley.edu>
2019-03-25 15:45:20 -0700

    Reconciling with kenny's branch

--- db/migrate/20190320222635_add_membership_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddMembershipToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :membership, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,12 @@ ActiveRecord::Schema.define(version: 2019_03_20_225557) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", default: "", null: false
-    t.string "membership", default: "", null: false
+    t.string "membership"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "membership"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
