9a41aa7c393287f1a7369e820b5f5ea83234d61b
Jason <jioneyang@berkeley.edu>
2019-04-07 23:55:37 +0000

    changed css for login page and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :calendars
+  
   get 'error/error'
   # get 'user/member_profile'
   devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'user/profile' => 'user#member_profile', :as => 'member_profile'
  
@@ -11,6 +13,8 @@ Rails.application.routes.draw do
   get 'user/calendar' => 'user#calendar', :as => 'user_calendar'
   
   get '/' => 'home#index', :as => 'root'
+  
+  
 
   root to: "home#index"
   # This needs to be at the end
