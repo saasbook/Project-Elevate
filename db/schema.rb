4e40924088d057164234b3f32a6d52a2c042ef5d
Jason <jioneyang@berkeley.edu>
2019-04-15 20:00:59 +0000

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_04_09_211313) do
 
-  create_table "coach_availabilities", force: :cascade do |t|
-    t.integer "coach_id", null: false
-    t.integer "player_id"
-    t.string "day", null: false
-    t.time "start_time", null: false
-    t.time "end_time", null: false
-  end
-
   create_table "calendars", force: :cascade do |t|
     t.string "name"
     t.integer "UserId"
@@ -27,6 +19,16 @@ ActiveRecord::Schema.define(version: 2019_04_09_211313) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "typeEvent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "coach_availabilities", force: :cascade do |t|
+    t.integer "coach_id", null: false
+    t.integer "player_id"
+    t.string "day", null: false
+    t.time "start_time", null: false
+    t.time "end_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
