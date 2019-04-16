962d1aa9b201bb319d1e278d42e43f3d28630e42
Jason <jioneyang@berkeley.edu>
2019-04-16 06:17:39 +0000

    change view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,18 @@ class HomeController < ApplicationController
 
     def index
         # redirect_to '/users/sign_in'
-        if current_user:
+        if current_user
             if current_user.membership == "Club Member" or current_user.membership == "Coach"
                 @calendars = Calendar.all.where(:UserId => [current_user.id, nil]).order(:start_time)
             else
                 @calendars = Calendar.all.order(:start_time)
             #add admin
             end
-        end
-        @calendarsShow = @calendars.limit(5)
-        @todayEvents = @calendars.all.where("start_time < ?", Time.now.end_of_day).where( "start_time > ?", Time.now.beginning_of_day).count
+             @calendarsShow = @calendars.limit(5)
+            @todayEvents = @calendars.all.where("start_time < ?", Time.now.end_of_day).where( "start_time > ?", Time.now.beginning_of_day).count
         
+        end
+       
     end
         
 
