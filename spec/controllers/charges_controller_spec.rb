6b35babec108f86a21948c0542eeeb721127cb0d
Joe Chen <chenjoe569@berkeley.edu>
2019-04-28 13:32:42 -0700

    Added controller test

---
+++ spec/controllers/charges_controller_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+# require 'spec_helper'
+# require 'charges_controller'
+
+# RSpec.describe ChargesController, type: :controller do
+#     describe "Checking User is a club member" do 
+
+
+#     end
+
+    
+# end
