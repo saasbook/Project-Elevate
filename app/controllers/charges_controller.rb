7cec8f2fb21a577c354168b6dd2bf1fccb6fe5bf
Joe Chen <chenjoe569@berkeley.edu>
2019-04-28 18:19:29 -0700

    rspec testing

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,6 +9,42 @@ class ChargesController < ApplicationController
       end
     end
 
+    def create_stripe_customer(email, stripe_token)
+      customer = Stripe::Customer.create({
+        email: email,
+        source: stripe_token,
+      })
+      return customer
+
+    end
+
+    def create_stripe_charges(amount, customer)
+      charge = Stripe::Charge.create({
+        customer: customer.id,
+        amount: amount,
+        description: 'Rails Stripe customer',
+        currency: 'usd',
+      })
+      return charge
+    end
+
+    def create_invoice(amount, customer)
+      Stripe::InvoiceItem.create({
+          amount: amount,
+          currency: 'usd',
+          customer: customer.id,
+          description: 'Lesson fee',
+      })
+      invoice = Stripe::Invoice.create({
+        customer: customer.id,
+        billing: 'send_invoice',
+        days_until_due: 30,
+      })
+
+      return invoice
+
+    end
+
     public 
     def new
       @possible_group_credits = ['0','1', '2','3', '4', '5', '6', '7', '8', '9', '10']
@@ -21,38 +57,13 @@ class ChargesController < ApplicationController
       @amount_in_create = params[:amount]
       @amount = params[:amount].to_f*100
       @amount = @amount.to_i
-    
-      customer = Stripe::Customer.create({
-        email: params[:stripeEmail],
-        # email: current_user.email,
-        source: params[:stripeToken],
-      })
       
-      charge = Stripe::Charge.create({
-        customer: customer.id,
-        amount: @amount,
-        # billing_details: {
-        #   city: params[:stripeBillingAddressCity],
-        #   country: params[:stripeBillingAddressCountry],
-        #   postal_code: params[:stripeBillingAddressZip],
-        #   state: params[:stripeBillingAddressState]
-        # },
-        description: 'Rails Stripe customer',
-        currency: 'usd',
-      })
-      Stripe::InvoiceItem.create({
-          amount: @amount,
-          currency: 'usd',
-          customer: customer.id,
-          description: 'Lesson fee',
-      })
-      invoice = Stripe::Invoice.create({
-        customer: customer.id,
-        billing: 'send_invoice',
-        days_until_due: 30,
-      })
+      customer = create_stripe_customer(params[:stripeEmail], params[:stripeToken])
+      charge = create_stripe_charges(@amount, customer)
+      invoice = create_invoice(@amount, customer)
+      #Actually sending customer invoices through email
       invoice.send_invoice
-      
+
       @custom_num_credit = params[:custom_num_credit].to_i + current_user.custom_num_credit.to_i
       @group_num_credit = params[:group_num_credit].to_i + current_user.group_num_credit.to_i
       @assigned_num_credit = params[:assigned_num_credit].to_i + current_user.assigned_num_credit.to_i
