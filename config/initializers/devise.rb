b829c1cd71ba4a64e5b5aec39fd760fdc0467f02
matthewsie <matthew.sie@berkeley.edu>
2019-04-29 13:43:00 -0700

    created and fixed different emails for new account vs. editing email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   config.mailer_sender = 'projectElevateDev@gmail.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'MyMailer'
+  config.mailer = 'AuthMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
