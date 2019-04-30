1cd0a116886bd5cdb7ff371bf007c8aa2f5cfaba
Kenneth <kahne@berkeley.edu>
2019-04-29 18:58:44 -0700

    added calendar fixes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,6 +18,8 @@ class CalendarsController < ApplicationController
 
   end
 
+
+
   # GET /calendars/1
   # GET /calendars/1.json
   def show
@@ -28,35 +30,25 @@ class CalendarsController < ApplicationController
   #   @calendar = Calendar.new
   # end
 
-  # GET /calendars/1
-  # GET /calendars/1.json
-  def show
-  end
-
-  # GET /calendars/new
-  def new
-    @calendar = Calendar.new
-  end
-
-  # GET /calendars/1/edit
-  def edit
-  end
+  # # GET /calendars/1/edit
+  # def edit
+  # end
 
   # POST /calendars
   # POST /calendars.json
   # def create
   #   @calendar = Calendar.new(calendar_params)
 
-    respond_to do |format|
-      if @calendar.save
-        format.html { redirect_to @calendar, notice: 'Calendar was successfully created.' }
-        format.json { render :show, status: :created, location: @calendar }
-      else
-        format.html { render :new }
-        format.json { render json: @calendar.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @calendar.save
+  #       format.html { redirect_to @calendar, notice: 'Calendar was successfully created.' }
+  #       format.json { render :show, status: :created, location: @calendar }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @calendar.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /calendars/1
   # PATCH/PUT /calendars/1.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,17 +18,17 @@ users.each do |user|
   User.create!(user)
 end
 
-calendars = [{:name => "Train with the Roger Destroyer", :UserId => 1, :OtherId => 2,  :typeEvent => "Coaching", :start_time => "2020-04-05 10:00:00", :end_time => "2020-04-05 12:00:00"},
-            {:name => "Coach Joe Chen", :UserId => 2, :OtherId => 1,  :typeEvent => "Coaching", :start_time => "2020-04-05 10:00:00", :end_time => "2020-04-05 12:00:00"},
-            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-11 10:00:00", :end_time => "2019-04-11 12:00:00"},
-            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-05 10:00:00", :end_time => "2019-04-05 12:00:00"},
-            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2020-04-13 10:00:00", :end_time => "2020-04-13 12:00:00"},
-            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00"},
-            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-21 10:00:00", :end_time => "2019-04-21 12:00:00"},
-            {:name => "Play with Joe Chen", :UserId => 5, :OtherId => 1,  :typeEvent => "Play", :start_time => "2019-04-23 10:00:00", :end_time => "2019-04-23 12:00:00"},
-            {:name => "Play with Jason Yang", :UserId => 1, :OtherId => 5,  :typeEvent => "Play", :start_time => "2019-04-23 10:00:00", :end_time => "2019-04-23 12:00:00"},
-            {:name => "Coach Jason Yang", :UserId =>2, :OtherId => 5, :typeEvent => "Coaching", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00"},
-            {:name => "Train with Roger Destroyer", :UserId =>5, :OtherId => 2, :typeEvent => "Coaching", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00"}]
+calendars = [{:name => "Train with the Roger Destroyer", :UserId => 1, :OtherId => 2,  :typeEvent => "Coaching", :start_time => "2020-04-05 10:00:00", :end_time => "2020-04-05 12:00:00", :event_day => "05", :event_month => "04"},
+            {:name => "Coach Joe Chen", :UserId => 2, :OtherId => 1,  :typeEvent => "Coaching", :start_time => "2020-04-05 10:00:00", :end_time => "2020-04-05 12:00:00", :event_day => "05", :event_month => "04"},
+            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-11 10:00:00", :end_time => "2019-04-11 12:00:00", :event_day => "11", :event_month => "04"},
+            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-05 10:00:00", :end_time => "2019-04-05 12:00:00", :event_day => "05", :event_month => "04"},
+            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2020-04-13 10:00:00", :end_time => "2020-04-13 12:00:00", :event_day => "13", :event_month => "04"},
+            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00", :event_day => "14", :event_month => "04"},
+            {:name => "Club Event", :UserId => nil, :OtherId => nil,  :typeEvent => "Event", :start_time => "2019-04-21 10:00:00", :end_time => "2019-04-21 12:00:00", :event_day => "21", :event_month => "04"},
+            {:name => "Play with Joe Chen", :UserId => 5, :OtherId => 1,  :typeEvent => "Play", :start_time => "2019-04-23 10:00:00", :end_time => "2019-04-23 12:00:00", :event_day => "23", :event_month => "04"},
+            {:name => "Play with Jason Yang", :UserId => 1, :OtherId => 5,  :typeEvent => "Play", :start_time => "2019-04-23 10:00:00", :end_time => "2019-04-23 12:00:00", :event_day => "23", :event_month => "04"},
+            {:name => "Coach Jason Yang", :UserId =>2, :OtherId => 5, :typeEvent => "Coaching", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00", :event_day => "14", :event_month => "04"},
+            {:name => "Train with Roger Destroyer", :UserId =>5, :OtherId => 2, :typeEvent => "Coaching", :start_time => "2019-04-14 10:00:00", :end_time => "2019-04-14 12:00:00", :event_day => "14", :event_month => "04"}]
 
 
 calendars.each do |calendar|
