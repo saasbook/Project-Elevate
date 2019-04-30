8c5b1c067b5e178fad2cc9c3fa6d8ccdd3782e02
Zac <zdehkordi@berkeley.edu>
2019-04-30 14:42:10 -0700

    fixed coach avail rspec

--- spec/models/coach_availability_spec.rb
+++ spec/models/coach_availability_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe CoachAvailability, type: :model do
   describe 'availibity functionality' do
     before(:each) do
-      User.create!({:name => 'Roger Destroyer', :email => 'bbb@gmail.com', :password => '12345678', :membership => 'Coach'})
+      User.create!({:id => 100, :name => 'Roger Destroyer', :email => 'bbb@gmail.com', :password => '12345678', :membership => 'Coach'})
       avails = [
-        {:coach_id => 1, :day => "Sunday", :start_time => '6am', :end_time => '9am'},
-        {:coach_id => 1, :day => "Sunday", :start_time => '12pm', :end_time => '3pm'},
-        {:coach_id => 1, :day => "Sunday", :start_time => '6pm', :end_time => '9pm'},
+        {:coach_id => 100, :day => "Sunday", :start_time => '6am', :end_time => '9am'},
+        {:coach_id => 100, :day => "Sunday", :start_time => '12pm', :end_time => '3pm'},
+        {:coach_id => 100, :day => "Sunday", :start_time => '6pm', :end_time => '9pm'},
       ]
       
       avails.each do |avail|
@@ -16,19 +16,19 @@ RSpec.describe CoachAvailability, type: :model do
     end
 
     it 'can add a valid time' do
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("9am"), Time.parse("12pm"))).to be(true)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("9am"), Time.parse("12pm"))).to be(true)
     end
 
     it 'cannot add an invalid time' do
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("6am"), Time.parse("8am"))).to be(false)
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("6am"), Time.parse("9am"))).to be(false)
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("6am"), Time.parse("7am"))).to be(false)
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("6am"), Time.parse("2pm"))).to be(false)
-      expect(CoachAvailability.valid_availibility(1, "Sunday", Time.parse("7am"), Time.parse("2pm"))).to be(false)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("6am"), Time.parse("8am"))).to be(false)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("6am"), Time.parse("9am"))).to be(false)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("6am"), Time.parse("7am"))).to be(false)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("6am"), Time.parse("2pm"))).to be(false)
+      expect(CoachAvailability.valid_availibility(100, "Sunday", Time.parse("7am"), Time.parse("2pm"))).to be(false)
     end
 
     it 'can return a coaches availability' do
-      sample = CoachAvailability.this_week(1)["Sunday"].pluck(:id)
+      sample = CoachAvailability.this_week(100)["Sunday"].pluck(:id)
       expect(sample).to include(1, 2, 3)
     end
   end
