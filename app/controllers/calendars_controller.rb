a8ee4b6a86a56e3434a30b2072e54956ec982172
Jason <jioneyang@berkeley.edu>
2019-05-12 03:48:24 +0000

    minor changes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -184,7 +184,6 @@ class CalendarsController < ApplicationController
       end
     end
     @calendar.destroy
-    
     respond_to do |format|
       format.html { redirect_to member_profile_path , notice: 'The event was successfully destroyed.' }
       format.json { head :no_content }
