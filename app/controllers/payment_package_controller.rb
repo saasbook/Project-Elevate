3f5f89e763cc160974b70ea8557dd1ba08d10da2
Zac <zdehkordi@berkeley.edu>
2019-05-12 04:38:25 -0700

    refactor create

--- app/controllers/payment_package_controller.rb
+++ app/controllers/payment_package_controller.rb
@@ -15,17 +15,17 @@ class PaymentPackageController < ApplicationController
     def create
         begin
             params[:payment_package].require([:name, :num_classes, :price])
-            if params[:payment_package][:name] == "Single"
-                flash[:alert] = "Can not have two Single packages"
-                redirect_to payment_package_path and return
-            elsif not PaymentPackage.find_by_num_classes(params[:payment_package][:num_classes]).blank?
-                flash[:alert] = "Can not create two packages with the same number of classes"
-                redirect_to payment_package_path and return
-            else
-                PaymentPackage.create!(params[:payment_package].permit(:name, :num_classes, :price))
-            end
         rescue => ex
             flash[:alert] = 'Missing fields or non-unqiue name'
+            redirect_to payment_package_path and return
+        end
+
+        if params[:payment_package][:name] == "Single"
+            flash[:alert] = "Can not have two Single packages"
+        elsif not PaymentPackage.find_by_num_classes(params[:payment_package][:num_classes]).blank?
+            flash[:alert] = "Can not create two packages with the same number of classes"
+        else
+            PaymentPackage.create!(params[:payment_package].permit(:name, :num_classes, :price))
         end
         
         redirect_to payment_package_path
