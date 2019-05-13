371bd210f1a8c5b3087b1665a90d0afae44f7b15
Kenneth <kahne@berkeley.edu>
2019-05-12 17:09:56 -0700

    added refactoring

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,8 +2,8 @@ class ChargesController < ApplicationController
     before_action :authenticate_user!
     before_action :check_current_user
     before_action :check_time_slot, only: [:checkout, :checkout_multiple]
-  
-    protected 
+
+    protected
     # Check if current user is a club member. If not, redirect them to their profile page
     def check_current_user
       if current_user.membership != "Club Member"
@@ -17,9 +17,9 @@ class ChargesController < ApplicationController
       if (params[:user].nil? || params[:user][:temp_availability].nil?)
         flash[:alert] = "Please choose a time slot."
         if params[:action] == "checkout_multiple"
-          redirect_to :controller => "user", :action => "multiple_booking" 
+          redirect_to :controller => "user", :action => "multiple_booking"
         else
-          redirect_to :controller => "user", :action => "booking" 
+          redirect_to :controller => "user", :action => "booking"
         end
         return
       end
@@ -78,7 +78,7 @@ class ChargesController < ApplicationController
       return false
     end
 
-    # Returns a list of [day, month, year] after increasing the day by 7(a week). 
+    # Returns a list of [day, month, year] after increasing the day by 7(a week).
     def update_day_month(day_index, month_index, year_index)
        # incrementing by 7 days and updating month and day and year
        day_index += 7
@@ -98,68 +98,62 @@ class ChargesController < ApplicationController
        end
     end
 
-    public 
+    public
     def new
     end
-    
+
     def create
       event_arr = []
       event_start_time, event_end_time = params[:event_start_time], params[:event_end_time]
       my_new_event_name, coach_new_event_name = "Lesson: #{event_start_time} to #{event_end_time}", "Coaching: #{event_start_time} to #{event_end_time}"
       # Storing booked lessons in the database for multiple booking
       if params[:multiple_booking] == "true"
-        num_classes = params[:num_classes].to_i
         month_index, day_index = params[:month_index].to_i, params[:day_index].to_i
         start_time_hour, end_time_hour = params[:start_time_hour].to_i, params[:end_time_hour].to_i
         start_time_minute, end_time_minute = params[:start_time_minute].to_i, params[:end_time_minute].to_i
         year_index = DateTime.now.year.to_i
-        for i in 1..num_classes do
+
+        for i in 1..params[:num_classes].to_i do
           event_start = Time.zone.local(year_index, month_index, day_index, start_time_hour, start_time_minute, 0)
           event_end = Time.zone.local(year_index, month_index, day_index, end_time_hour, end_time_minute, 0)
-  
-          temp_type_event, conflict = "Coaching", "No Conflict"
-          if !(Booking.check_time_slot(event_start, event_end, params[:coach_id].to_i, [month_index, day_index, year_index]))
-            conflict = "Conflict"
-          end
-          
-          my_new_event = Calendar.new(:name => my_new_event_name, :UserId => current_user.id, :OtherId => params[:coach_id].to_i, :start_time => event_start, :end_time => event_end, :typeEvent => temp_type_event, :event_month => month_index.to_s, :event_day => day_index.to_s, :conflict => conflict)
-          coach_new_event = Calendar.new(:name => coach_new_event_name, :UserId => params[:coach_id].to_i, :OtherId => current_user.id, :start_time => event_start, :end_time => event_end, :typeEvent => temp_type_event, :event_month => month_index.to_s, :event_day => day_index.to_s,  :conflict => conflict)
-          event_arr << my_new_event
-          event_arr << coach_new_event
-  
+
+          event_arr << Calendar.create_event([event_start, event_end, month_index, day_index, year_index], [current_user.id, params[:coach_id].to_i, params[:coach_id].to_i], [my_new_event_name, "Coaching"], true)
+          event_arr << Calendar.create_event([event_start, event_end, month_index, day_index, year_index], [params[:coach_id].to_i, current_user.id, params[:coach_id].to_i], [coach_new_event_name, "Coaching"], true)
+
+
+
           # incrementing by 7 days and updating month and day
           day_index, month_index, year_index = update_day_month(day_index, month_index, year_index)
         end
       else
          # Storing booked lesson in the database for single booking
-        my_new_event = Calendar.new(:name => my_new_event_name, :UserId => current_user.id, :OtherId => params[:coach_id].to_i, :start_time => params[:event_start], :end_time => params[:event_end], :typeEvent => "Coaching", :event_month => params[:month], :event_day => params[:day])
-        coach_new_event = Calendar.new(:name => coach_new_event_name, :UserId => params[:coach_id].to_i, :OtherId => current_user.id, :start_time => params[:event_start], :end_time => params[:event_end], :typeEvent => "Coaching", :event_month => params[:month], :event_day => params[:day])
-        event_arr << my_new_event
-        event_arr << coach_new_event
+
+        event_arr << Calendar.create_event([params[:event_start], params[:event_end], params[:month], params[:day]], [current_user.id, params[:coach_id].to_i, params[:coach_id].to_i], [my_new_event_name, "Coaching"], false)
+        event_arr << Calendar.create_event([params[:event_start], params[:event_end], params[:month], params[:day]], [params[:coach_id].to_i, current_user.id, params[:coach_id].to_i], [coach_new_event_name, "Coaching"], false)
       end
       # Amount in cents
       @amount_in_create = params[:amount]
       @amount = params[:amount].to_f*100
       @amount = @amount.to_i
-      
+
       customer = create_stripe_customer(params[:stripeEmail], params[:stripeToken])
       charge = create_stripe_charges(@amount, customer)
       invoice = create_invoice(@amount, customer)
       #Actually sending customer invoices through email
       invoice.send_invoice
       #Actually save in database
-      event_arr.each do |event| 
+      event_arr.each do |event|
         event.save!
 
       end
 
-      
-      
+
+
 
       rescue Stripe::CardError => e
         flash[:error] = e.message
         redirect_to new_charge_path
-    
+
     end
 
 
@@ -173,7 +167,7 @@ class ChargesController < ApplicationController
       @month = params[:month]
       @day = params[:day]
       @coach = params[:coach_id]
-      
+
       #Checking if it's invalid time (namely if the booked time is before current time). If it is, then flash error
       if check_time_past(@event_start, false, DateTime.now.year.to_i)
         return
@@ -181,7 +175,7 @@ class ChargesController < ApplicationController
 
       @number_hours = 1
       @amount = PaymentPackage.single_class_price
-  
+
     end
 
     # Checkout controller for multiple booking
@@ -194,13 +188,13 @@ class ChargesController < ApplicationController
       @event_start = Time.zone.local(day_info[2], day_info[1], day_info[0], start_time.hour, start_time.minute, 0)
       @event_end = Time.zone.local(day_info[2], day_info[1], day_info[0], end_time.hour, end_time.minute, 0)
 
-      # Checking if it's invalid time (namely if the booked time is before current time). If it is, then flash error 
+      # Checking if it's invalid time (namely if the booked time is before current time). If it is, then flash error
       if check_time_past(Time.zone.local(day_info[2], day_info[1], day_info[0], start_time.hour, start_time.minute, 0), true, day_info[2])
         return
       end
 
       lessons, conflicting_lessons = get_lessons(@num_classes, day_info, start_time, end_time)
-      
+
       @start_time_hour, @start_time_minute, @end_time_hour, @end_time_minute = start_time.hour, start_time.minute, end_time.hour, end_time.minute
       @coach_id, @multiple_booking = params[:coach_id], "true"
       @amount, @lessons = PaymentPackage.payment_package_price_by_num_class(@num_classes), lessons
@@ -221,12 +215,12 @@ class ChargesController < ApplicationController
     def get_lessons(num_classes, day_info, start_time, end_time)
       conflicting_lessons, lessons = "", ""
       day_index, month_index, year_index = day_info
-      
+
       for i in 1..num_classes do
         event_start = Time.zone.local(year_index, month_index, day_index, start_time.hour, start_time.minute, 0)
         event_end = Time.zone.local(year_index, month_index, day_index, end_time.hour, end_time.minute, 0)
         lessons += append_lesson(i, num_classes, month_index, day_index)
-        
+
         # Checking if there are conflicted lessons and add the dates into a string
         if !(Booking.check_time_slot(event_start, event_end, params[:coach_id].to_i, [month_index, day_index, year_index]))
           conflicting_lessons += Date::MONTHNAMES[month_index] + " " + day_index.to_s + ", "
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,13 @@
 class Calendar < ApplicationRecord
+  def self.create_event(event_info, ids, event_name_type, conflict_switch)
+    if (conflict_switch)
+      conflict = "No Conflict"
+      if !(Booking.check_time_slot(event_info[0], event_info[1], ids[2], [event_info[2], event_info[3], event_info[4]]))
+        conflict = "Conflict"
+      end
+      return Calendar.new(:name => event_name_type[0], :UserId => ids[0], :OtherId => ids[1], :start_time => event_info[0], :end_time => event_info[1], :typeEvent => event_name_type[1], :event_month => event_info[2].to_s, :event_day => event_info[3].to_s, :conflict => conflict)
+    else
+      return Calendar.new(:name => event_name_type[0], :UserId => ids[0], :OtherId => ids[1], :start_time => event_info[0], :end_time => event_info[1], :typeEvent => event_name_type[1], :event_month => event_info[2].to_s, :event_day => event_info[3].to_s)
+    end
+  end
 end
