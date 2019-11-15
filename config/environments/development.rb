41178c68ef853035b62714596cfbabeb9014defe
Jagminder Shergill <jjthelordoftheworld@gmail.com>
2019-11-14 16:59:36 -0800

    User Login Fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,20 +2,21 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Confirm user account email
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = 
   {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'gmail.com', #you can also use google.com
-    :authentication       => :plain,
     :user_name            => 'projectElevateDev@gmail.com',
-    :enable_starttls_auto => true,
     :password             => 'LetsElevate!'
+    :authentication       => :plain,
+    :enable_starttls_auto => true,
   }
+
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
