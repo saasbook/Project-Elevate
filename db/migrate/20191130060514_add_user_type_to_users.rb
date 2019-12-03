4ab019b86ed621d7e6f95e247deb84f1793d88a1
Ardrian Wong <ardrianwong@berkeley.edu>
2019-12-02 19:12:07 -0800

    Schema user_type attribute default value bug fixed

--- db/migrate/20191130060514_add_user_type_to_users.rb
+++ db/migrate/20191130060514_add_user_type_to_users.rb
@@ -1,5 +1,5 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_type, :string
+    add_column :users, :user_type, :string,  default: "Student", null: false
   end
 end
