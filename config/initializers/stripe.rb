97cea6fcb16a0c30a4deeddccde6c24680acc620
Jagminder Shergill <jshergill6828@berkeley.edu>
2019-12-13 22:41:48 -0800

    Heroku Enviorment

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.publish_key_stripe,
-  :secret_key      =>  Rails.application.credentials.secret_key_stripe 
+  :publishable_key => Rails.application.credentials.publish_key_stripe || ENV['PUBLISHABLE_KEY'],
+  :secret_key      =>  Rails.application.credentials.secret_key_stripe || ENV['SECRET_KEY']
 
 }
 
